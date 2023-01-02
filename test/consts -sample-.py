943d494b0ef11f605feaf4a282029f2abf559c1e
dapalex <alessandro.daprile@outlook.com>
2023-01-02 20:17:18 +0700

    Create consts -sample-.py

---
+++ test/consts -sample-.py
@@ -0,0 +1,19 @@
+gdbcHeaders = {
+  "content-type": "application/json", ##it needs to be added for graphQl version
+	"X-RapidAPI-Key": "<your API key here>",
+	"X-RapidAPI-Host": "geodb-cities-graphql.p.rapidapi.com"
+}
+
+gdbcUrl = "https://geodb-cities-graphql.p.rapidapi.com/"
+
+githubHeaders = {"Authorization": '<your API key here>'}
+
+githubUrl = "https://api.github.com/graphql"
+
+rapidApiHeaders = {
+        "content-type": "application/json", ##it needs to be added for graphQl version
+	    "X-RapidAPI-Key":  "<your API key here>",
+	    "X-RapidAPI-Host": "graphql-rapidapi-test.p.rapidapi.com"
+        }
+
+rapidApiUrl = 'https://graphql-rapidapi-test.p.rapidapi.com/'
