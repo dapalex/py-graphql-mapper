f56ff5994a262d52d95e4562995690a7671ab485
dapalex <alessandro.daprile@outlook.com>
2023-01-07 23:29:37 +0700

    update WF names

--- codegen/utils.py
+++ codegen/utils.py
@@ -4,27 +4,27 @@ import os
 import pathlib
 
 
-class OutputType(Enum):
-    COMMANDS = 1,
-    SCHEMAS = 2,
-    OTHER = 3
+# class OutputType(Enum):
+#     COMMANDS = 1,
+#     SCHEMAS = 2,
+#     OTHER = 3
 
-def cleanFolder(folder):
-    for top, dirs, files in os.walk(folder):
-        for file in files:
-            os.remove(os.path.join(top,file))
-        for dir in dirs:
-            directory = os.path.join(folder, dir)
-            cleanFolder(directory)
-            os.rmdir(directory)
+# def cleanFolder(folder):
+#     for top, dirs, files in os.walk(folder):
+#         for file in files:
+#             os.remove(os.path.join(top,file))
+#         for dir in dirs:
+#             directory = os.path.join(folder, dir)
+#             cleanFolder(directory)
+#             os.rmdir(directory)
             
-def cleanOutput(outType: OutputType):
-    if outType == OutputType.COMMANDS:
-        dirToClean = pathlib.Path(os.path.join(os.curdir, 'commandsOutput'))
-    if outType == OutputType.SCHEMAS:
-        dirToClean = pathlib.Path(os.path.join(os.path.join(os.curdir, 'test'), 'output'))
+# def cleanOutput(outType: OutputType):
+#     if outType == OutputType.COMMANDS:
+#         dirToClean = pathlib.Path(os.path.join(os.curdir, 'commandsOutput'))
+#     if outType == OutputType.SCHEMAS:
+#         dirToClean = pathlib.Path(os.path.join(os.path.join(os.curdir, 'test'), 'output'))
     
-    cleanFolder(dirToClean)
+#     cleanFolder(dirToClean)
         
-def waitForInput(isToWait: str):
-    if isToWait and isToWait.lower() == 'y': input('Go on..')
+# def waitForInput(isToWait: str):
+#     if isToWait and isToWait.lower() == 'y': input('Go on..')
