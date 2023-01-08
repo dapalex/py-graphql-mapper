45e307780891301132a08f91f3b0ba8c915aff71
dapalex <alessandro.daprile@outlook.com>
2023-01-08 15:40:53 +0700

    update for publish
    - Update pyproject.ml
    - Update setup.py
    - Update READMEs

--- setup.py
+++ setup.py
@@ -3,21 +3,15 @@ from setuptools import setup
 setup(
     name='pygqlmap',
     version='1.0.0',
-    description='A python library to call GraphQL APIs without using hardcoded strings',
-    long_description= open('README.MD').read(),
+    long_description= open('README.md').read(),
     url='https://github.com/dapalex/py-graphql-mapper/',
     author='Alex Dap',
     author_email='shlisi2017@gmail.com',
-    license='LICENSE.txt',
+    description='A python library to call GraphQL APIs without using hardcoded strings',
     include_package_data=True,
     packages=['pygqlmap', 'pygqlmap.src', 'codegen', 'codegen.src', 'codegen.src.templates'],
     data_files=[('', ['pygqlmap/config.ini'])],
-    python_requires='>=3.9',
-    entry_points ={
-            'console_scripts': [
-                'pgmcodegen = codegen.__main__:main'
-            ]
-        },
+    python_requires='>=3.10',
     classifiers=[
     "Development Status :: 4 - Beta",
         'Intended Audience :: Developers',
