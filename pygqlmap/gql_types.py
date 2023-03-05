857dc31ff9f98bc913c05b3d8df2111e4ef9a631
Alex Dap <shlisi2017@gmail.com>
2023-03-06 03:47:45 +0700

    Update gql_types.py

--- pygqlmap/gql_types.py
+++ pygqlmap/gql_types.py
@@ -4,5 +4,4 @@ class NonNull_float(float): pass
 class NonNull_bool(int): pass
 class NonNull_str(str): pass
 class NonNull_ID(ID): pass
-class NonNull_Bool(int): pass
 class NonNull_list(list): pass
