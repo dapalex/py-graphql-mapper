faa5f1884ccbbed76b6302fae64e14ecfc295179
dapalex <shlisi2017@gmail.com>
2023-07-02 15:23:31 +0100

    pypi publish
    set new publish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pygqlmap',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/dapalex/py-graphql-mapper/',
     author='Alex Dap',
     author_email='shlisi2017@gmail.com',
@@ -12,7 +12,6 @@ setup(
     data_files=[('', ['pygqlmap/config.ini'])],
     python_requires='>=3.10',
     classifiers=[
-    "Development Status :: 4 - Beta",
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
